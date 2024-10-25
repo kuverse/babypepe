db4d592f0d9a4973411c8e7ef8f3b50bfb44136a
kuverse <kucollectionnft@gmail.com>
2024-10-25 13:38:22 +0700

    update

--- src/components/MarketCap.tsx
+++ src/components/MarketCap.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { Box, Text, Spinner, Flex, Image } from "@chakra-ui/react";
 
@@ -54,44 +56,43 @@ const MarketCapVolume: React.FC = () => {
     >
       {loading ? (
         <Flex justifyContent="center" alignItems="center" height="100%">
-        <Spinner size="lg" color="white" /> {/* Set spinner color to white */}
-      </Flex>
+          <Spinner size="lg" color="white" /> {/* Set spinner color to white */}
+        </Flex>
       ) : (
         <Flex
           className="ticker"
-          width="100%" // Make the ticker take the full width
           whiteSpace="nowrap"
-          animation="scroll 10s linear infinite" // Increase duration for slower scrolling
+          animation="scroll 15s linear infinite" // Adjusted duration for smoother scrolling
           fontFamily="'SecondaryFont', sans-serif"
         >
-          {/* Original content with more instances */}
-          {[...Array(10)].map((_, i) => (
+          {/* Duplicate the content for endless scroll effect */}
+          {[...Array(2)].map((_, i) => (
             <React.Fragment key={i}>
               <Flex direction={"row"} alignItems="center" mx={5}>
                 <Image src="/logos/fbe8b5cf8780a6a5f6ff3678e60ed349.avif" height={5} width={5} alt="Market Cap Logo" />
                 <Text fontSize="2xl" fontWeight="bold" color="white" mx={2}>
-                Market Cap: ${parseFloat(marketCap).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                  Market Cap: ${parseFloat(marketCap).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
                 </Text>
               </Flex>
 
               <Flex direction={"row"} alignItems="center" mx={5}>
                 <Image src="/images/xcircle-logo.png" height={5} width={5} alt="Token Price Logo" />
                 <Text fontSize="2xl" fontWeight="bold" color="white" mx={2}>
-                Token Price: ${parseFloat(usdValue).toLocaleString(undefined, { minimumFractionDigits: 8, maximumFractionDigits: 8 })}
+                  Token Price: ${parseFloat(usdValue).toLocaleString(undefined, { minimumFractionDigits: 8, maximumFractionDigits: 8 })}
                 </Text>
               </Flex>
 
               <Flex direction={"row"} alignItems="center" mx={5}>
                 <Image src="/images/white-circle-logo.png" height={5} width={5} alt="Volume Logo" />
                 <Text fontSize="2xl" fontWeight="bold" color="white" mx={2}>
-                24hr Volume: ${parseFloat(volumeValue).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                  24hr Volume: ${parseFloat(volumeValue).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
                 </Text>
               </Flex>
 
               <Flex direction={"row"} alignItems="center" mx={5}>
                 <Image src="/images/ethLogo.png" height={5} width={8} alt="Ethereum Price Logo" />
                 <Text fontSize="2xl" fontWeight="bold" color="white" mx={2}>
-                Eth Price: ${parseFloat(ethPrice).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                  Eth Price: ${parseFloat(ethPrice).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
                 </Text>
               </Flex>
             </React.Fragment>
@@ -104,11 +105,12 @@ const MarketCapVolume: React.FC = () => {
             transform: translateX(0);
           }
           100% {
-            transform: translateX(-100%);
+            transform: translateX(-100%); /* Change to -100% to show all text */
           }
         }
         .ticker {
           display: inline-flex; /* Ensures flex items align in a row */
+          padding: 0; /* Adjust padding as necessary */
         }
       `}</style>
     </Box>
