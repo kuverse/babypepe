8252fed3dfb98313ce27ff9fe53efd7ab7184a4a
kuverse <kucollectionnft@gmail.com>
2024-11-20 09:53:59 +0700

    update

--- src/components/AboutBox.tsx
+++ src/components/AboutBox.tsx
@@ -122,7 +122,7 @@ const AboutBox: React.FC = () => {
               textShadow="0.5px 0.5px 0 black, -0.5px 0.5px 0 black, 0.5px -0.5px 0 black, -0.5px -0.5px 0 black"
 
             >
-              Babypepe, heir to the throne and son of  Pepe, represents the dawn of a new generation in the frog kingdom, and a passing of the baton, from one generation to the next.  Signifying the rite of passage bestowed upon a son by his father, Babypepe strives to build and improve upon his dad&apos;s legacy.
+              BabyPepe, heir to the throne and son of  Pepe, represents the dawn of a new generation in the frog kingdom, and a passing of the baton, from one generation to the next.  Signifying the rite of passage bestowed upon a son by his father, Babypepe strives to build and improve upon his dad&apos;s legacy.
               Pepe, the king of memes and a viral sensation found all over the internet, has taken the world by storm, and captivated the hearts of many.  If you missed Pepe&apos;s parabolic run, Babypepe is an opportunity to be a part of something bigger than yourself, and have a hand in building a community that will surely go down in memecoin history.
               With Pepe being found in every corner of the globe, it&apos;s only a matter of time before Babypepe makes his mark on the world!
 
@@ -149,7 +149,7 @@ const AboutBox: React.FC = () => {
               textShadow="0.5px 0.5px 0 black, -0.5px 0.5px 0 black, 0.5px -0.5px 0 black, -0.5px -0.5px 0 black"
 
             >
-              The Babypepe narrative is simple.  Son of Pepe, whose CA starts with 0x69, Babypepe boasts a unique vanity contract address similar to that of his father, starting with 0x69babe.  A CTO, by the people, for the people, was forged back in May of 2024, with no shortage of obstacles in its path.  A community of diamond-handed, cult-like chads, has defeated two cabal launches, dethroned a serial scammer crypto twitter influencer, and built an organic following of dedicated indviduals who are committed to the growth and success of this adorable little frog.
+              The BabyPepe narrative is simple.  Son of Pepe, whose CA starts with 0x69, Babypepe boasts a unique vanity contract address similar to that of his father, starting with 0x69babe.  A CTO, by the people, for the people, was forged back in May of 2024, with no shortage of obstacles in its path.  A community of diamond-handed, cult-like chads, has defeated two cabal launches, dethroned a serial scammer crypto twitter influencer, and built an organic following of dedicated indviduals who are committed to the growth and success of this adorable little frog.
               Come stop by our Telegram, check out the Twitter, and see for yourself.  With a foundation of integrity, honesty, kindness, and compassion, everyone is valued and has a seat at the table in our community.
             </Text>
           </Bubble>
